a8ef334381bb735d8cbc6b9997969686a802c357
Mohamed-Raafat <m7mdraafat2003@gmail.com>
2025-11-29 01:22:44 +0200

    - Implement Generate JWT token function - Remove unneeded function.

--- src/Bokra.Core/Interfaces/IJwtTokenService.cs
+++ src/Bokra.Core/Interfaces/IJwtTokenService.cs
@@ -1,3 +1,6 @@
+using Bokra.Core.DTOs.Auth.Response;
+using Bokra.Core.Entities.Identity;
+
 namespace Bokra.Core.Interfaces;
 
 public interface IJwtTokenService
@@ -6,13 +9,6 @@ public interface IJwtTokenService
     /// Generates a JWT token for the given user
     /// </summary>
     /// <param name="user">User to generate token for</param>
-    /// <returns>JWT Token string</returns>
-    // string GenerateToken(User user);
-
-    /// <summary>
-    /// Validates a JWT token and returns whether it is valid
-    /// </summary>
-    /// <param name="token">JWT Token string</param>
-    /// <returns>True if token is valid; otherwise, false</returns>
-    bool ValidateToken(string token);
+    /// <returns>JWT Token Result</returns>
+    Task<TokenResult> GenerateTokenAsync(User user);
 }
