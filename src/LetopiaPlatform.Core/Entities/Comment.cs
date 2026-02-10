5c99985a69748bd783ae16c41350a7f4d3b0895b
Mohamed Raafat <mohamedraafat.engineer@gmail.com>
2026-02-10 23:40:35 +0200

    refactor(entities): enhance data integrity by updating foreign key relationships and enforcing required properties

--- src/LetopiaPlatform.Core/Entities/Comment.cs
+++ src/LetopiaPlatform.Core/Entities/Comment.cs
@@ -1,5 +1,6 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using LetopiaPlatform.Core.Entities.Identity;
 
 namespace LetopiaPlatform.Core.Entities
 {
@@ -10,28 +11,26 @@ namespace LetopiaPlatform.Core.Entities
         [Column("id")]
         public Guid Id { get; set; }
 
-        // FK to Post
         [Required]
         [Column("post_id")]
         public Guid PostId { get; set; }
 
-        //[ForeignKey("PostId")]
-        //public Post Post { get; set; }
+        [ForeignKey("PostId")]
+        public Post Post { get; set; } = null!;
 
-        // FK to User (Author)
         [Required]
         [Column("author_id")]
         public Guid AuthorId { get; set; }
 
-        //[ForeignKey("AuthorId")]
-        //public User Author { get; set; }
+        [ForeignKey("AuthorId")]
+        public User Author { get; set; } = null!;
 
         [Required]
         [Column("content")]
-        public string Content { get; set; }
+        public required string Content { get; set; }
 
         [Column("upvotes")]
-        public int Upvotes { get; set; } = 0;
+        public int Upvotes { get; set; }
 
         [Column("created_at")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
@@ -39,7 +38,4 @@ namespace LetopiaPlatform.Core.Entities
         [Column("updated_at")]
         public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
     }
-
-
-
 }
--- src/LetopiaPlatform.Core/Entities/UserCommunity.cs
+++ src/LetopiaPlatform.Core/Entities/UserCommunity.cs
@@ -1,7 +1,6 @@
-using System;
-using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
-
+using System.ComponentModel.DataAnnotations.Schema;
+using LetopiaPlatform.Core.Entities.Identity;
 
 namespace LetopiaPlatform.Core.Entities
 {
@@ -12,21 +11,19 @@ namespace LetopiaPlatform.Core.Entities
         [Column("id")]
         public Guid Id { get; set; }
 
-        // FK to Users
         [Required]
         [Column("user_id")]
         public Guid UserId { get; set; }
 
-        //[ForeignKey("UserId")]
-        //public User User { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; } = null!;
 
-        // FK to Communities
         [Required]
         [Column("community_id")]
         public Guid CommunityId { get; set; }
 
-        //[ForeignKey("CommunityId")]
-        //public Community Community { get; set; }
+        [ForeignKey("CommunityId")]
+        public Community Community { get; set; } = null!;
 
         [Column("joined_at")]
         public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
