5164d161de2f3b3bcf27c36a929e8c99c2ef9972
Mohamed Raafat <mohamedraafat.engineer@gmail.com>
2026-02-13 19:00:11 +0200

    feat(infra): register Group DbSet in ApplicationDbContext
    Refs #60

--- src/LetopiaPlatform.Infrastructure/Data/ApplicationDbContext.cs
+++ src/LetopiaPlatform.Infrastructure/Data/ApplicationDbContext.cs
@@ -19,7 +19,8 @@ public class ApplicationDbContext : IdentityDbContext<User, Role, Guid>
     public DbSet<Post> Posts => Set<Post>();
     public DbSet<Comment> Comments => Set<Comment>();
     public DbSet<UserCommunity> UserCommunities => Set<UserCommunity>();
-
+    public DbSet<Group> Groups => Set<Group>();
+    
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
