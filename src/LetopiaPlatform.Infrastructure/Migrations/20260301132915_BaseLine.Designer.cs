63e2a61d19da8a113d4c7038de66eff065da1975
RafatShepl <rafatshepl17@gmail.com>
2026-03-03 18:40:53 +0200

    Feat/110 add reaction rntity and enum (#161)

--- src/LetopiaPlatform.Infrastructure/Migrations/20260301132915_BaseLine.Designer.cs
+++ src/LetopiaPlatform.Infrastructure/Migrations/20260301132915_BaseLine.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using LetopiaPlatform.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace LetopiaPlatform.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260301132915_BaseLine")]
+    partial class BaseLine
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -493,10 +496,6 @@ namespace LetopiaPlatform.Infrastructure.Migrations
                         .HasColumnType("boolean")
                         .HasDefaultValue(false)
                         .HasColumnName("is_pinned");
-
-                    b.Property<int>("PostImageUrl")
-                        .HasColumnType("integer")
-                        .HasColumnName("Post_image_url");
 
                     b.Property<string>("PostType")
                         .IsRequired()
