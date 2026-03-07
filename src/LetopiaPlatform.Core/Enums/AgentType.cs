4a4b3317fed0df2f160ef1d35122c906b509b5dc
MohhamedIbrahem <almorshdymohamed161@gmail.com>
2026-03-07 17:17:56 +0200

    refactor: serialize AgentType enum as string using JsonStringEnumConverter

--- src/LetopiaPlatform.Core/Enums/AgentType.cs
+++ src/LetopiaPlatform.Core/Enums/AgentType.cs
@@ -1,8 +1,12 @@
+
+using System.Text.Json.Serialization;
+
 namespace LetopiaPlatform.Core.Enums;
 
 /// <summary>
 /// Defines the types of AI agents available in the platform.
 /// </summary>
+[JsonConverter(typeof(JsonStringEnumConverter))]
 public enum AgentType
 {
     /// <summary>
